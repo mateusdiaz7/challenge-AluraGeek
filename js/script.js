e0fe388b0b90a1b55521fc8d4709f20232e4e95c
mateusdiaz7 <mateuzdias.pubg@gmail.com>
2024-05-13 17:50:15 -0300

    inicio do projeto

---
+++ js/script.js
@@ -0,0 +1,18 @@
+const url = "http://localhost:3000/produtos";
+const secaoProdutos = document.querySelector(".secao-produtos");
+
+async function pegarProdutos() {
+    const response = await fetch(url);
+    const data = await  response.json();
+
+    console.log(data);
+
+    data.map((produto) => {
+        const secao = document.createElement("section");
+        secao.className("secao-produtos")
+        const div = document.createElement("div");
+
+    })
+}
+
+pegarProdutos();
